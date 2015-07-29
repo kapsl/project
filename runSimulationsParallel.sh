1b86bef557572b0bac86980e2851a94db8d300da
Manuel <kaspar.manuel@posteo.de>
2015-07-29 17:53:57 +0200

    RunSimulation AODV

--- runSimulationsParallel.sh
+++ runSimulationsParallel.sh
@@ -2,12 +2,13 @@
 #
 
 # First parameter gives first border, second gives second
+# The String AODVNetwork is, when the AODV Simulation should run
 
 echo "Running Simulations ..."
 
 for ((i = $1; i < $2; i++))
 do
-	./NetworkStructures -r $i -u Cmdenv -n .:../inet/examples:../inet/src -l ../inet/src/inet omnetpp.ini &
+	./NetworkStructures -r $i -u Cmdenv -n AODVNetwork .:../inet/examples:../inet/src -l ../inet/src/inet omnetpp.ini &
 done 
 
 wait
